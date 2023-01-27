27f9fbb7801930a185a07919cc4863dcf65977d4
fgonzalezr1998 <fergonzaramos@yahoo.es>
2023-01-27 21:29:54 +0100

    [minecraft] forge updated to 1.19.2. WIP

--- minecraft/context/entrypoint.sh
+++ minecraft/context/entrypoint.sh
@@ -2,5 +2,5 @@
 
 cd /home/fernando/minecraft_server
 echo 'eula=true' > eula.txt
-java -Xmx2048M -jar forge-1.16.4-35.1.4.jar nogui > log &
+#java -Xmx2048M -jar forge-1.16.4-35.1.4.jar nogui &
 /bin/bash
--- minecraft/run.sh
+++ minecraft/run.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-docker run --rm -it --name fercraft --hostname fercraft fgonzalezr1998/fercraftserver
+docker run --rm -it --name fercraft --hostname fercraft \
+--volume /home/fernando/Github_Repos/Minecraft_Server:/home/fernando/Minecraft_Server \
+fgonzalezr1998/fercraftserver
